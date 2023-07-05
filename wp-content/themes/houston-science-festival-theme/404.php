0cc130a9edc2d82d9ba22e01335844f8655573c2
cmcoke <minoru.cm@gmail.com>
2023-07-05 16:46:36 -0500

    created the present page hero section

--- wp-content/themes/houston-science-festival-theme/404.php
+++ wp-content/themes/houston-science-festival-theme/404.php
@@ -1,18 +1,14 @@
-<?php get_header() ?>
+<div class='relative bg-[var(--blue]'>
+  <div class="absolute -translate-y-2/4 w-full text-center left-0 top-2/4">
 
-<div class="absolute -translate-y-2/4 w-full text-center left-0 top-2/4">
+    <div class='flex flex-col gap-5 px-4'>
+      <h1 class="text-9xl font-semibold">Oops</h1>
+      <h2 class='text-2xl'>404 - PAGE NOT FOUND</h2>
+      <p class='text-lg'>The page you are looking for might have been removed, <br> had it's name changed or is
+        temporarily unavailable.
+      </p>
+      <a href=" <?php echo esc_url(site_url('/')); ?>" class='button'>GO TO HOMEPAGE</a>
+    </div>
 
-
-  <div class='flex flex-col gap-5 px-4'>
-    <h1 class="text-9xl font-semibold">Oops</h1>
-    <h2 class='text-2xl'>404 - PAGE NOT FOUND</h2>
-    <p class='text-lg'>The page you are looking for might have been removed, <br> had it's name changed or is
-      temporarily unavailable.
-    </p>
-    <a href=" <?php echo esc_url(site_url('/')); ?>" class='button'>GO TO HOMEPAGE</a>
   </div>
-
-
 </div>
-
-<?php get_footer() ?>
